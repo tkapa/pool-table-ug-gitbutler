6f6508af9782b43662988fc692149ed249d70dde
Tylah Kapa <tylahkapa@gmail.com>
2024-11-20 19:53:17 +1000

    feat: implement DoSomething class and SayHelloWorld method
    Adds a new class DoSomething with a method SayHelloWorld that 
    currently throws a NotImplementedException. This change sets up 
    the structure for future functionality related to greeting 
    messages.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+var doSomething = new DoSomething();
+doSomething.SayHelloWorld();
+
+public class DoSomething
+{
+    public void SayHelloWorld() => throw new NotImplementedException();
+}
